7de3441b2b44ea4bff2da798ba76819e78e23752
Ganiev Konstantin <socingin@ya.ru>
2022-08-24 16:33:05 +0400

    code task №4 without input

--- Seminar 1/Task №4/Program.cs
+++ Seminar 1/Task №4/Program.cs
@@ -1,1 +1,17 @@
-﻿int num1 = 1, num2 = 2, num3 = 3, max = 0;
+﻿int num1 = 10, num2 = 20, num3 = 3, max = 0;
+if (num1 >= num2 && num1 >= num3)
+{
+    max = num1;
+}
+else
+{
+    if (num2 >= num3)
+    {
+        max = num2;
+    }
+    else
+    {
+        max = num3;
+    }
+}
+Console.Write (max);
