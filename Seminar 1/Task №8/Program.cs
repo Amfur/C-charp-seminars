4df2d6a97ceddef775a223d91304b738004dcca6
Ganiev Konstantin <socingin@ya.ru>
2022-08-24 17:42:36 +0400

    code task №8 without input

---
+++ Seminar 1/Task №8/Program.cs
@@ -0,0 +1,21 @@
+﻿int N = -181, even_num = 2;
+if (N > 1)
+{
+    while (even_num <= N)
+    {
+        if (even_num == 2)
+        {
+            Console.Write("\n" + even_num);
+        }
+        else
+        {
+            Console.Write(" " + even_num);
+        }
+        even_num = even_num + 2;
+    }
+    Console.Write("\n");
+}
+else
+{
+    Console.WriteLine("\nМежду введёным числом и 1 отсутствуют чётные числа");
+}
