b59c2b6ea3224d5aa3f51ec03953bbcbf4d69d21
Ganiev Konstantin <socingin@ya.ru>
2022-08-24 16:05:15 +0400

    code without input

--- Seminar 1/Task №2/Program.cs
+++ Seminar 1/Task №2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1 = 7, num2 = 7, max = 0;
+if (num1 >= num2 )
+{
+    max = num1;
+}
+else
+{
+    max = num2;
+}
+Console.Write (max);
