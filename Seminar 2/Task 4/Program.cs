5e36a5a8bf56cee14e2a735fa6313e815cc694a0
Ganiev Konstantin <socingin@ya.ru>
2022-09-09 20:14:10 +0400

    add code Task 4

---
+++ Seminar 2/Task 4/Program.cs
@@ -0,0 +1,11 @@
+﻿int start = 1, finish = 1000000;
+int randomNumber = new Random().Next(start, finish + 1);
+Console.WriteLine(randomNumber);
+if (randomNumber % 7 == 0 && randomNumber % 23 == 0)
+{
+    Console.WriteLine("Сгенерированное число одновременно кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine("Сгенерированное число не является одновременно кратным 7 и 23");
+}
