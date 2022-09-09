f39614e85ef6a53a3b1b2576f7cd65000ec273bd
Ganiev Konstantin <socingin@ya.ru>
2022-09-09 19:28:20 +0400

    add code Task 3

---
+++ Seminar 2/Task 3/Program.cs
@@ -0,0 +1,16 @@
+﻿int dayNumber = Convert.ToInt32(Console.ReadLine());
+if (0 < dayNumber && dayNumber < 8)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Будний день");
+}
+}
+else
+{
+    Console.WriteLine("Введённое число не является номером дня недели");
+}
