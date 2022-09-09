6b7d2de52255c3e81d6de1ce7b8966d997a1002a
Ganiev Konstantin <socingin@ya.ru>
2022-09-09 19:10:45 +0400

    add code Task 2

---
+++ Seminar 2/Task 2/Program.cs
@@ -0,0 +1,12 @@
+﻿int start = 1, finish = 65536;
+int randomNumber = new Random().Next(start, finish + 1);
+Console.WriteLine(randomNumber);
+int thridDigit = randomNumber/100;
+if (thridDigit == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(thridDigit%10);
+}
