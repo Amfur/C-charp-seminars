9aa9c744937d9bd37a5a24b26a14872e6962277f
Ganiev Konstantin <socingin@ya.ru>
2022-09-15 19:39:40 +0400

    add code Task 3

--- Seminar 3/Task 3/Program.cs
+++ Seminar 3/Task 3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+if (0 < N && N < 10)
+{
+    int[] cube = new int[N];
+    for (int count = 1; count <= N; count++)
+    {
+        cube[count-1] = count*count*count;
+        Console.WriteLine(cube[count-1]);
+    }
+}
+else
+{
+    Console.WriteLine("Введено ненатуральное число");
+}
