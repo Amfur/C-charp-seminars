2f0cd2b985e9b23644863adb2d02d1c6b520b415
Ganiev Konstantin <socingin@ya.ru>
2022-09-15 18:13:47 +0400

    add code Task 1

--- Seminar 3/Task 1/Program.cs
+++ Seminar 3/Task 1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = Convert.ToInt32(Console.ReadLine()); // Пользователь вводит число
+if (number / 10000 != 0 && number / 100000 == 0) // Проверка числа на пятизначность
+{
+    if (number/10000 == number%10 && number/1000%10 == number/10%10) // Сравнение 5-го разряда с 1-ым и 4-го со 2-ым
+    {
+        Console.WriteLine("Палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Введено не пятизначное число");
+}
