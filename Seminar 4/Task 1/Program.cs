69ec8a03946c5a26fdbf30f5224d93a9f59a7c80
Ganiev Konstantin <socingin@ya.ru>
2022-09-19 14:33:51 +0400

    add code Task 1

--- Seminar 4/Task 1/Program.cs
+++ Seminar 4/Task 1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число A:");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите натуральное число B:");
+int B = Convert.ToInt32(Console.ReadLine());
+double invol = A;
+if (0 < B)
+{
+    for(int mult = 1; mult < B; mult++)
+    {
+        invol *= A;
+    }
+    Console.WriteLine("A в степени B = " + invol);
+}
+else
+{
+    Console.WriteLine("Введёное число B не является натуральным");
+}
