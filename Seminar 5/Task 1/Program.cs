6193093460a2e72d998c9e8475b4a81544f11eda
Ganiev Konstantin <socingin@ya.ru>
2022-09-22 15:57:04 +0400

    add code S5/Task 1

--- Seminar 5/Task 1/Program.cs
+++ Seminar 5/Task 1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Массив на сколько элементов создать?");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] threeDigit = new int[size];
+int start = 100, finish = 1000, evenNumbers = 0;
+for(int index = 0; index < size; index++)
+{
+    threeDigit[index] = new Random().Next(start, finish);
+    if (threeDigit[index]%2 == 0) evenNumbers++;
+    Console.WriteLine(threeDigit[index]);
+}
+Console.WriteLine("Колличество чётных чисел в массиве: " + evenNumbers);
