e0a332221e7c2bdebd703603bee8097bacbad44f
Ganiev Konstantin <socingin@ya.ru>
2022-10-04 22:17:58 +0400

    add code S6/T1

--- Seminar 6/Task 1/Program.cs
+++ Seminar 6/Task 1/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿Console.WriteLine("Введите натуральное число:");
+int M = Convert.ToInt32(Console.ReadLine());
+if(M <= 0)
+{
+    Console.WriteLine("Введено ненатуральное число!");
+}
+else
+{
+    int positiveNum = 0;
+    int tmp;
+    for(int num = 1; num <= M; num++)
+    {
+        Console.WriteLine("Введите " + num + "-е число:");
+        tmp = Convert.ToInt32(Console.ReadLine());
+        if(tmp > 0)
+        {
+            positiveNum++;
+        }
+    }
+    Console.WriteLine("Введено чисел больше 0: " + positiveNum);
+}
