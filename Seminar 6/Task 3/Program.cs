69f8475bcd8dd932b19e47567b55c336e7dc6e15
Ganiev Konstantin <socingin@ya.ru>
2022-10-05 14:17:37 +0400

    add code S6/T3

--- Seminar 6/Task 3/Program.cs
+++ Seminar 6/Task 3/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.WriteLine("Задайте размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] nums = new int[size];
+Console.WriteLine();
+for(int i = 0; i < size; i++)
+{
+    nums[i] = new Random().Next(1,10);
+    Console.WriteLine(nums[i]);
+}
+int sum = 0;
+for(int index = 0; index < size / 2; index++)
+{
+    sum = sum + nums[index] * nums[size-1-index];
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма произведений пар чисел заданного массива равна {sum}");
