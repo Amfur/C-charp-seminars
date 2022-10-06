d9ba2a5c8313954b58fa595f01bda28ed0d5b56e
Ganiev Konstantin <socingin@ya.ru>
2022-10-06 18:24:15 +0400

    add code S7/T2

--- Seminar 7/Task 2/Program.cs
+++ Seminar 7/Task 2/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿//  Создание массива случайной размерности и наполнение его случайными целыми числами от -99 до 99
+Random rnd = new Random();
+int a = rnd.Next(2, 5);
+int b = rnd.Next(2, 5);
+int[,] exampleArray = new int[a, b];
+for (int i = 0; i < exampleArray.GetLength(0); i++)
+{
+    for (int j = 0; j < exampleArray.GetLength(1); j++)
+    {
+        exampleArray[i, j] = rnd.Next(-99, 100);
+        Console.Write(exampleArray[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+//  Ввод номера строки и стобца. Поиск значения указанного элемента.
+//  Т.к в повседневной жизни никто не говорит "нулевой столбец" или "нулевая строка"
+//  нумерацию решил сделать с единицы
+Console.Write("Введите номера строки и столбца нужного элемента массива\nСтрока: ");
+int str = Convert.ToInt32(Console.ReadLine());
+Console.Write("Столбец: ");
+int col = Convert.ToInt32(Console.ReadLine());
+
+if (str < 1 || str > exampleArray.GetLength(0) ||
+    col < 1 || col > exampleArray.GetLength(1))
+    {
+        Console.WriteLine("В массиве нет элемента с указанными индексами");
+    }
+else
+    {
+        Console.WriteLine(exampleArray[str-1,col-1]);
+    }
